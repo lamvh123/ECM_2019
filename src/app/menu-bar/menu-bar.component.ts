13cb7f960ffc6f53fd701f00a7105fa74fc3a375
= <=>
2019-06-16 23:05:00 +0700

    asd

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ProfileComponent } from '../profile/profile.component';
-declare var jquery: any; declare var $: any;
+import * as $ from 'jquery'; 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
@@ -17,25 +17,59 @@ export class MenuBarComponent implements OnInit, AfterViewInit {
   name;
   ngOnInit() {
     this.name = this.className();
+  //   $(document).ready(function() {
+  //     alert('ok');
+  //     // $() will work as an alias for jQuery() inside of this function
+  //     $.getScript("/assets/bundles/libscripts.bundle.js", function (data, textStatus, jqxhr) {
+  //       console.log( textStatus );
+  //     });
+  //     console.log("ok1");
+  //     $.getScript("/assets/bundles/vendorscripts.bundle.js", function (data, textStatus, jqxhr) {
+  //       console.log( textStatus );
+  //     });
+  //     console.log("ok1");
+  //     $.getScript("/assets/bundles/mainscripts.bundle.js", function (data, textStatus, jqxhr) {
+  //       console.log( textStatus );
+  //     });
+  //     console.log("ok1");
+  //      $('.dtp-content').hide();
+     
+  // });
   }
   className():String{
     return this._router.url;
   }
-
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
   ngAfterViewInit() {
-    $.getScript("/assets/bundles/libscripts.bundle.js", function (data, textStatus, jqxhr) {
-      console.log( textStatus );
-    });
-    console.log("ok1");
-    $.getScript("/assets/bundles/vendorscripts.bundle.js", function (data, textStatus, jqxhr) {
-      console.log( textStatus );
-    });
-    console.log("ok1");
-    $.getScript("/assets/bundles/mainscripts.bundle.js", function (data, textStatus, jqxhr) {
-      console.log( textStatus );
-    });
-    console.log("ok1");
-     $('.dtp-content').hide();
-    console.log('1' + this._router.url);
+    this.loadScript('/assets/bundles/libscripts.bundle.js');
+     this.loadScript('/assets/bundles/vendorscripts.bundle.js');
+     this.loadScript('/assets/bundles/mainscripts.bundle.js');
+  //   $(document).ready(function() {
+  //     alert('ok');
+  //     // $() will work as an alias for jQuery() inside of this function
+  //     $.getScript("/assets/bundles/libscripts.bundle.js", function (data, textStatus, jqxhr) {
+  //       console.log( textStatus );
+  //     });
+  //     console.log("ok1");
+  //     $.getScript("/assets/bundles/vendorscripts.bundle.js", function (data, textStatus, jqxhr) {
+  //       console.log( textStatus );
+  //     });
+  //     console.log("ok1");
+  //     $.getScript("/assets/bundles/mainscripts.bundle.js", function (data, textStatus, jqxhr) {
+  //       console.log( textStatus );
+  //     });
+  //     console.log("ok1");
+  //      $('.dtp-content').hide();
+     
+  // });
+   
    }
 }
